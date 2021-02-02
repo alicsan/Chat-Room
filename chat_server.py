cafc6e971794294f3fbc80166967f51d2b31ea9f
alicsan <aliciacardenosa.s@gmail.com>
2021-02-02 20:00:57 +0100

    improving the server

--- chat_server.py
+++ chat_server.py
@@ -1,35 +1,74 @@
 import socket
-import time
-import sys
+import select
 
-new_socket = socket.socket()
+HEADER_LENGHT = 10
+
 host_name = socket.gethostname()
-s_ip = socket.gethostbyname(host_name)
+server_ip = socket.gethostbyname(host_name)
 port = 1234
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((server_ip, port))
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
 
 
-new_socket.bind((str(s_ip), port))
-print("Binding successful!")
-print("This is your IP:", s_ip)
+def received_message(client_socket):
+	try:
+		message_header = client_socket.recv(HEADER_LENGHT)
 
-name=input("Enter name:")
-new_socket.listen(1)
+		if not len(message_header):
+			return False
+		
+		message_length = int(message_header.decode().strip())
 
-conn,add = new_socket.accept()
-print("Recieved connection from", add[0])
-print("Conection Established. Connected from: ",add[0])
+		#we return a dict with the client info
+		return {"header":message_header,"data":client_socket.recv(message_length)}
 
-client = (conn.recv(1024)).decode()
-print(client + "has connected.")
-conn.send(name.enconde())
+	except:
+		return False
 
 while True:
-    message = input('Me: ')
-    conn.send(message.enconde())
-    message = conn.recv(1024)
-    message = message.decode()
-    print(client + ":" + message) 
+	read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
+	for notified_socket in read_sockets:
+		if notified_socket == server_socket:
+			client_socket, client_address = server_socket.accept()
+
+			user = received_message(client_socket)
+			if user is False:
+				continue
+			sockets_list.append(client_socket)
+
+			clients[client_socket]  = user
+
+			print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode()}")
+
+
+		else:
+			message = received_message(notified_socket)
+
+			if message is False:
+				print(f"Closed connection from  {clients[notified_socket]['data'].decode()}")
+				sockets_list.remove(notified_socket)
+				del clients[notified_socket]
+				continue
+
+			user = clients[notified_socket]
+			print (f"Recieved message from {user['data'].decode()}: {message['data'].decode()}")
+
+			for client_socket in clients:
+				if client_socket != notified_socket:
+					client_socket.send(user['header'] + user['data'] + message['header'] +  message['data'])
+
+
+	for notified_socket in exception_sockets:
+		sockets_list.remove(notified_socket)
+		del clients[notified_socket]
 
 
